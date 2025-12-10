a52045cb3c466c417459cbe0d7dcc5c086f80df4
Elias Orlow <orlow.elias03@gmail.com>
2025-12-10 12:43:53 +0100

    init folder structur

--- src/main/java/org/elias/Main.java
+++ src/main/java/org/elias/Main.java
@@ -1,7 +1,16 @@
 ﻿package org.elias;
 
+/**
+ * Enthaelt die {@code main()} methode.
+ *
+ * @author Elias
+ */
 public class Main
 {
+    /**
+     * Enthaelt alle notwendigen Methoden, um das Programm zu starten.
+     *
+     */
     static void main ()
     {
 
