dcc8e8fc63dcf3a9b962e1e6c660cb1da04a1644
Elias Orlow <orlow.elias03@gmail.com>
2026-01-03 10:47:49 +0100

    refactor(CSVFileReader): add documentation comment and better error handling

--- src/main/java/org/elias/res/constant/ErrorMessages.java
+++ src/main/java/org/elias/res/constant/ErrorMessages.java
@@ -5,5 +5,7 @@ public interface ErrorMessages
     String MAIN_CONTROLLER_NOT_INITIALIZED = "MainController wurde noch nicht initialisiert!";
     String MAIN_CONTROLLER_ALREADY_INITIALIZED = "MainController wurde schon initialisiert!";
 
+    String ERROR_READING_CSV = "Fehler beim Lesen der CSV-Datei: ";
+
     String NULL_COORDINATES = "Coordinates cannot be null";
 }
--- src/main/java/org/elias/util/CSVFileReader.java
+++ src/main/java/org/elias/util/CSVFileReader.java
@@ -1,19 +1,35 @@
 package org.elias.util;
 
+import org.elias.res.constant.ErrorMessages;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Util-Klasse zum Einlesen einer CSV-Datei und Umwandeln in eine Liste von Zeilen.
+ */
 public class CSVFileReader
 {
+    /**
+     * Liest eine CSV-Datei zeilenweise ein und gibt alle Datenzeilen als Liste zurueck.
+     * <p>
+     * Die erste Zeile (Header) wird ignoriert.
+     *
+     * @param pathToCSV Pfad zur CSV-Datei.
+     * @return Eine Liste aller Datenzeilen der CSV-Datei ohne Header.
+     * @precondition Der angegebene Pfad verweist auf eine existierende gueltige Datei.
+     * @postcondition bei Fehlern wird eine leere Liste geliefert.
+     */
     public static List<String> convertCSVtoList (String pathToCSV)
     {
         List<String> dataRows = new ArrayList<>();
 
         try (BufferedReader input = new BufferedReader(new FileReader(pathToCSV)))
         {
+            // Erste Zeile (Header) ueberspringen
             String dataLine = input.readLine();
 
             while ((dataLine = input.readLine()) != null)
@@ -23,7 +39,7 @@ public class CSVFileReader
 
         } catch (IOException e)
         {
-            e.printStackTrace();
+            System.err.println(ErrorMessages.ERROR_READING_CSV + e.getMessage());
         }
 
         return dataRows;
